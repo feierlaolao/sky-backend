22949c15f3b0bf12ce3d0b6aa55f69ed40dfc2f4
nunu <nunu@gmail.com>
2025-09-13 15:31:18 +0700

    修改部分内容

--- app/Service/Inv/ItemService.php
+++ app/Service/Inv/ItemService.php
@@ -24,9 +24,9 @@ class ItemService
             ->when(isset($data['merchant_id']), fn(Builder $query) => $query->where('merchant_id', $data['merchant_id']))
             ->when(isset($data['category_id']), fn(Builder $query) => $query->where('category_id', $data['category_id']))
             ->when(isset($data['name']), fn(Builder $query) => $query->where('name', 'like', '%' . $data['name'] . '%'))
-            ->with(['skus' => function (Builder $query) {
-                $query->whereNull('base_sku_id');
-            }, 'skus.price', 'skus.children', 'category', 'brand', 'images.attachment'])
+            ->with(['skus' => function ($query) {
+                $query->whereNull('base_sku_id')->with(['price', 'children']);
+            }, 'category', 'brand', 'images.attachment'])
             ->paginate(perPage: $data['pageSize'] ?? 20, page: $data['current'] ?? 1);
     }
 
