16b065dbfb4260aa0110af2d0094e7037324cd21
nunu <nunu@gmail.com>
2025-09-14 15:22:32 +0700

    增加进货日期

--- app/Service/Inv/ItemService.php
+++ app/Service/Inv/ItemService.php
@@ -41,13 +41,13 @@ class ItemService
     {
         return InvItemSpu::query()->where('merchant_id', $data['merchant_id'])
             ->when(!empty($data['name']), fn(Builder $query) => $query->where('name', 'like', '%' . $data['name'] . '%'))
-            ->with('skus')
+            ->with('skus.price')
             ->whereHas('skus.price', function ($query) use ($data) {
                 if (!empty($data['channel_id'])) {
-                    $query->where('channel_id', $data['channel_id'])->with(['price']);
+                    $query->where('channel_id', $data['channel_id']);
                 }
                 if (!empty($data['barcode'])) {
-                    $query->where('barcode', $data['barcode'])->with(['price']);
+                    $query->where('barcode', $data['barcode']);
                 }
             })
             ->paginate(perPage: $data['page_size'] ?? 20, page: $data['current'] ?? 1);
